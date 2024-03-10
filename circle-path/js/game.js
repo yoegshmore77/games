bd879877b621c93174d275dcb942d1a961b840da
yoegshmore77 <48826311+yoegshmore77@users.noreply.github.com>
2024-03-10 06:39:17 +0530

    Add files via upload

--- circle-path/js/game.js
+++ circle-path/js/game.js
@@ -106,7 +106,8 @@ playGame.prototype = {
       this.addTarget();
     }
 
-    this.resize();
+    //this.resize();
+    //resize1();
     
 
   },
@@ -204,4 +205,22 @@ playGame.prototype = {
         canvas.style.height = windowHeight + "px";
     }
 }
+}
+
+// pure javascript to scale the game
+function resize1() {
+  alert("called");
+    var canvas = document.querySelector("canvas");
+    var windowWidth = window.innerWidth;
+    var windowHeight = window.innerHeight;
+    var windowRatio = windowWidth / windowHeight;
+    var gameRatio = game.config.width / game.config.height;
+    if(windowRatio < gameRatio){
+        canvas.style.width = windowWidth + "px";
+        canvas.style.height = (windowWidth / gameRatio) + "px";
+    }
+    else{
+        canvas.style.width = (windowHeight * gameRatio) + "px";
+        canvas.style.height = windowHeight + "px";
+    }
 }
